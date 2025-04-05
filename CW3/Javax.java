6acbbfab70bf7da46ec98e0105fc40f6b0bdc664
User <User@LAPTOP>
2025-04-05 16:29:13 +0200

    a

--- CW3/Javax.java
+++ CW3/Javax.java
@@ -1,5 +1,20 @@
 package CW3;
 
 public class Javax {
+
+   
+
+     static void mine(int[]n)
+    {
+        System.out.println("Siema");
+
+    }
+
+    static void main(String[] args)
+    {
+        int[] liczby={1,2,3};
+        mine(liczby);
+    }
+    
     
 }

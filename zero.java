cdef4e39caa364e47834d861fe39273a25c4c8a1
User <User@LAPTOP>
2025-04-05 15:51:04 +0200

    cw3

---
+++ zero.java
@@ -0,0 +1,18 @@
+public class zero {
+    int number = 6;
+   
+    public static void say(String... Words){
+        for(String n:Words){
+            System.out.println("I say " + n + ". ");
+    
+        }
+    }
+}
+
+
+
+
+
+
+
+    
